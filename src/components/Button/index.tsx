d1612cc854292cb21da4712b1c79ce13b7461184
Vinicius Bastos <vclemente2@hotmail.com>
2023-06-27 21:59:18 -0300

    Implementação do contador.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,16 @@ import style from "./Button.module.scss";
 interface ButtonProps {
   children: string;
   type?: "button" | "submit" | "reset" | undefined;
+  onClick?: () => void;
 }
 
-export function Button({ children, type }: ButtonProps): JSX.Element {
+export function Button({ children, type, onClick }: ButtonProps): JSX.Element {
   return (
-    <button type={type ? type : "button"} className={style.botao}>
+    <button
+      onClick={onClick}
+      type={type ? type : "button"}
+      className={style.botao}
+    >
       {children}
     </button>
   );
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -17,13 +17,28 @@ export function Timer({ selectedTask }: TimerProps) {
     if (selectedTask?.selected) setTime(0);
   }, [selectedTask]);
 
+  function countdown(countdownTime: number = 0) {
+    setTimeout(() => {
+      if (countdownTime > 0) {
+        setTime(countdownTime - 1);
+        return countdown(countdownTime - 1);
+      }
+    }, 1000);
+  }
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha uma tarefa e inicie o cronometro.</p>
       <div className={style.relogioWrapper}>
         <Clock time={time} />
       </div>
-      <Button>Começar!</Button>
+      <Button
+        onClick={() => {
+          countdown(time);
+        }}
+      >
+        Começar!
+      </Button>
     </div>
   );
 }
